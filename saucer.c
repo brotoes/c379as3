ab3fb63bef33a09f81f5558507af273472aece4c
Brock Toews <Brock@Brocks-MacBook-Pro.local>
2013-11-21 11:27:36 -0700

    procedural progress

--- saucer.c
+++ saucer.c
@@ -2,49 +2,61 @@
 #include <stdio.h>
 #include <ncurses.h>
 #include <pthread.h>
-#include <ioctl.h>
+#include <sys/ioctl.h>
 #include "saucer.h"
 
 static struct saucer_data * saucers;
 static struct missile_data * missiles;
-static struct drawer_data to_draw;
-static pthread_t * thread_ids;
+static pthread_t last_thread_id;
 static pthread_barrier_t * global_barrier;
 
 int nproc;
 int new_missile_ind;
 int missiles;
 int escaped;
+int launcher_x;
+int saucer_rate;
+int score;
+struct winsize win;	
 
 int
 main(int argc, char * argv[])
 {
+	int saucer_counter = 0;
 	int playing = 1;
 	char * drawbuf;
-	struct winsize win;	
 
 	/*initialize everything*/
 	ioctl(0, TIOCGWINSZ, &win);
 	drawbuf = malloc(win.ws_row * win.ws_col * sizeof(char));
 	nproc = get_nprocs();
 	pthread_setconcurrency(nproc);
+	missiles = START_MISSILES;
+	launcher_x = win.ws_col/2;
+	saucer_rate = START_SAUCER_RATE;
+	score = 0;
 
 	/*init launcher thread*/
-	pthread_create(thread_ids, );
+	pthread_create(&last_thread_id, NULL, launcher_init, NULL);
 
 	/*start synchronization loop*/
 	while(playing) {
-		/*reset any per loop global variables*/
+		/*perform action on any per loop global variables*/
 		new_missile_ind = -1;
+		saucer_counter = (saucer_counter + 1) % saucer_rate;
 		/*Spawn saucers*/
 		pthread_barrier_wait(global_barrier);
-		/*movement of missles and saucers*/
+
+		/*wait for movement of missles and saucers*/
 		pthread_barrier_wait(global_barrier);
-		/*player interaction*/
+
+		/*wait for player interaction*/
 		pthread_barrier_wait(global_barrier);
+
 		/*collision checks if missile spawned*/
 		/*put sprites into draw buffer*/
 		pthread_barrier_wait(global_barrier);
+
 		/*draw everything to the screen*/
 		/*wait for next timer step*/
 		pthread_barrier_wait(global_barrier);
@@ -54,16 +66,21 @@ main(int argc, char * argv[])
 }
 
 void *
-launcher_init(void * data) {
+launcher_init(void * data) 
+{
 	/*wait for saucers to spawn*/
 	pthread_barrier_wait(global_barrier);
+
 	/*wait for saucers and missiles to move*/
 	pthread_barrier_wait(global_barrier);
+
 	/*TODO:perform player interactions*/
 	pthread_barrier_wait(global_barrier);
+
 	/*wait for collision checks*/
 	/*Wait for sprites to be put into draw buffer*/
 	pthread_barrier_wait(global_barrier);
+
 	/*wait for end of loop actions*/
 	pthread_barrier_wait(global_barrier);
 }
@@ -71,14 +88,20 @@ launcher_init(void * data) {
 void *
 saucer_init(void * data) 
 {
-	/*wait for saucers to spawn*/
+	/*perform actions of per loop local variables*/
+	data.ttl --;
 	pthread_barrier_wait(global_barrier);
-	/*TODO:move self*/
+
+	/*move self*/
+	data.x += data.speed;
 	pthread_barrier_wait(global_barrier);
+
 	/*wait for player interactions*/
 	pthread_barrier_wait(global_barrier);
+
 	/*TODO:check for collisions if necessary*/
 	pthread_barrier_wait(global_barrier);
+
 	/*wait for end of loop actions*/
 	pthread_barrier_wait(global_barrier);
 }
@@ -86,14 +109,20 @@ saucer_init(void * data)
 void *
 missile_init(void * data) 
 {
-	/*wait for saucers to spawn*/
+	/*perform actions on per loop local variables*/
+	data.ttl --;
 	pthread_barrier_wait(global_barrier);
-	/*TODO:move self*/
+
+	/*move self*/
+	data.y -= MISSILE_SPEED;
 	pthread_barrier_wait(global_barrier);
+
 	/*perform player interactions*/
 	pthread_barrier_wait(global_barrier);
+
 	/*wait for collision checks*/ 
 	pthread_barrier_wait(global_barrier);
+
 	/*wait for end of loop actions*/
 	pthread_barrier_wait(global_barrier);
 	
--- saucer.h
+++ saucer.h
@@ -6,20 +6,22 @@
 #define MISSILE_SPEED 5
 #define SAUCER_SPEED_TOP 4
 #define SAUCER_SPEED_LOW 1
-
-struct launcher_data
+#define START_MISSILES 10
+#define START_SAUCER_RATE 5
+#define GAME_SPEED 30
 
 struct missile_data {
 	int x;
 	int y;
-}
+    int ttl;
+};
 
 struct saucer_data {
 	int x;
 	int y;
-	int dead;
+    int ttl;
 	int speed;
-}
+};
 
 void * launcher_init(void *);
 void * saucer_init(void *);
